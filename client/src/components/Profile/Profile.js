f0402071e41cf075d39682d88092e7fafae56fa1
demo835 <demo835@yahoo.com>
2018-08-17 14:49:51 -0400

    more materialize work

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "./Profile.css"
 
 class Profile extends Component {
     constructor() {
@@ -12,25 +13,51 @@ class Profile extends Component {
     render() {
         return (
             <div>
+                <div className="row">
+            <div className="col s12 m6">
+              <div className="card white">
+                <div className="card-content black-text">
                 <p>Condition: {this.props.response}</p>
                 <p id="id">ID: {this.props.id}</p>
-                {/* <form onSubmit={event => this.handleSubmit.bind(this)}>
-                    <input type="text" id="condition" name="condition"/>
-                    <button type="submit">Create Condition</button>
-                </form> */}
+
                 <form>
                     <input type="text" id="create"></input>
-                    <button onClick={() => this.props.createCondition()}>Create Condition</button>
+                    <i className="material-icons prefix">add_circle_outline</i>
+                    <button className="optionButtons btn
+ waves-effect waves-light #e3f2fd blue lighten-4 col s12" onClick={() => this.props.createCondition()}>Create Condition</button>
                 </form>
                 <form>
                     <input type="text" id="update"></input>
-                    <button onClick={() => this.props.updateCondition()}>Update Condition</button>
+                    <i className="material-icons prefix">edit</i>
+                    <button className="optionButtons btn
+ waves-effect waves-light #b3e5fc light-blue lighten-3 col s12" onClick={() => this.props.updateCondition()}>Update Condition</button>
                 </form>
                 <form>
                     <input type="text" id="delete"></input>
-                    <button onClick={() => this.props.deleteCondition()}>Delete Condition</button>
+                    <i className="material-icons prefix">delete</i>
+                    <button className="optionButtons btn
+ waves-effect waves-light #81d4fa light-blue lighten-2 col s12" onClick={() => this.props.deleteCondition()}>Delete Condition</button>
                 </form>
+                </div>
+                </div>
+                </div>
+                </div>
             </div>
+        //     <div class="row">
+        //     <div class="col s12 m6">
+        //       <div class="card blue-grey darken-1">
+        //         <div class="card-content white-text">
+        //           <span class="card-title">Card Title</span>
+        //           <p>I am a very simple card. I am good at containing small bits of information.
+        //           I am convenient because I require little markup to use effectively.</p>
+        //         </div>
+        //         <div class="card-action">
+        //           <a href="#">This is a link</a>
+        //           <a href="#">This is a link</a>
+        //         </div>
+        //       </div>
+        //     </div>
+        //   </div>
         )
     }
 }
